fc67d5dc24ba3e9e83b8c028c642db5d41ba1aec
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-03 15:12:29 +0600

    Math function added

---
+++ math.js
@@ -0,0 +1,27 @@
+// const number = -4;
+var myNumber = Math.abs(number);
+// console.log(myNumber);
+
+// const number = 12.48345;
+var myNumber = Math.ceil(number);
+// console.log(myNumber);
+
+// const number = 12.48345;
+var myNumber = Math.floor(number);
+// console.log(myNumber);
+
+// const number1 = 12.48345;
+// const number2 = 12.58345;
+var myNumber1 = Math.round(number1);
+// var myNumber2 = Math.round(number2);
+// console.log(myNumber1);
+// console.log(myNumber2);
+
+
+// var myNumber = Math.ceil( Math.random() * 8 );
+// console.log(myNumber);
+
+for (var i = 1; i <= 20; i++) {
+    var myNumber = Math.ceil(Math.random() * 8);
+    console.log(myNumber);
+}
